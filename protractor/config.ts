262c0064cf5a191a59a87ee35b5aaac44c6543fa
PSLCOL\lzapata <lzapata@psl.com.co>
2018-02-24 18:09:39 -0500

    Solución problema dependencias

---
+++ protractor/config.ts
@@ -0,0 +1,14 @@
+import { browser, Config } from 'protractor';
+import { reporter }   from './helpers/reporter';
+
+export const config: Config = {
+  framework: 'jasmine',
+  SELENIUM_PROMISE_MANAGER: false,
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 1000,
+  noGlobals: true,
+  onPrepare: () => {
+    browser.ignoreSynchronization = true;
+    reporter();
+  }
+};
